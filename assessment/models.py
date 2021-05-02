59e55cc81bf2740b447907c92a8c046eeb4cfb83
Tawfiq-Abubakar <tawfiqabubakr7@gmail.com>
2021-05-02 00:52:45 +0000

    first commit

---
+++ assessment/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class Bosco(models.Model):
+    name = models.CharField(max_length=256)
+
+class AbdulQadir(models.Model):
+    pass
+
+class Ubaidullah(models.Models):
+    pass 
+
+class Issah(models.Model):
+    pass 
+
+class Subburu(models.Model):
+    pass 
+
+class Ihsan(models.Model):
+    pass 
+
+class Ziblim(models.Model):
+    pass
+
+class AbdulGhaffar(models.Model):
+    pass
